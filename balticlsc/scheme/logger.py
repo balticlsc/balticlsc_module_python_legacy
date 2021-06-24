2a011a2d0bc72dcb2b644a9032f63bacac8a2e2d
Kacper Jarczak <48131726+kjarczak@users.noreply.github.com>
2021-06-24 10:00:00 +0200

    Update logger.py
    Updated logger to match BLSC documentation

--- balticlsc/scheme/logger.py
+++ balticlsc/scheme/logger.py
@@ -1,3 +1,3 @@
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG, format='## SERVER.TASKMGR ## %(asctime)s.%(msecs)03d ## %(levelname)s|%(name)s: %(message)s', datefmt='%d.%m.%Y %H:%M:%S')
 logger = logging.getLogger('api')
