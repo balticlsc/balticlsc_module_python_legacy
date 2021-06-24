f9f9067fa10806629b5ac3a03a8bcd46e1cab188
Kacper Jarczak <48131726+kjarczak@users.noreply.github.com>
2021-06-24 10:43:32 +0200

    Update logger.py

--- balticlsc/scheme/logger.py
+++ balticlsc/scheme/logger.py
@@ -1,3 +1,6 @@
 import logging
-logging.basicConfig(level=logging.DEBUG, format='## SERVER.TASKMGR ## %(asctime)s.%(msecs)03d ## %(levelname)s|%(name)s: %(message)s', datefmt='%d.%m.%Y %H:%M:%S')
+import os
+
+SYS_MODULE_NAME = os.getenv('SYS_MODULE_NAME', 'BalticLSC module')
+logging.basicConfig(level=logging.DEBUG, format='## '+SYS_MODULE_NAME+' ## %(asctime)s.%(msecs)03d ## %(levelname)s|%(name)s: %(message)s', datefmt='%d.%m.%Y %H:%M:%S')
 logger = logging.getLogger('api')
